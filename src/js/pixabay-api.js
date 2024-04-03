fb70088fbf0e5b7ed1192c89a96c05d48d20110b
NowAlime <147753857+NowAlime@users.noreply.github.com>
2024-04-03 17:15:08 +0300

    add

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-export async function fetchImage(query, currentPage) {
+export async function fetchImage( inputValue, currentPage) {
   const BASE_URL = "https://pixabay.com/api/"
   const params = {
     key: "43075807-09317d918fe027bf1568df9fe",
-    q: query,
+    q: inputValue,
     image_type: "photo",
     orientation: "horizontal",
     safesearch: true,
